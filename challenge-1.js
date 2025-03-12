1529547f681329b27500c209b107a52483c40d02
Yvonne-del <ys359375@gmail.com>
2025-03-12 05:59:39 +0300

    create a function in challenge-1.js that grades students according to the marks they provide

---
+++ challenge-1.js
@@ -0,0 +1,22 @@
+//creates a function for grading marks provided
+function studentMarks(marks){
+    if(marks < 40){
+        return "Grade E";
+    }
+    else if(marks >=40 && marks < 50){
+        return "Grade D";
+    }
+    else if(marks >=50 && marks <60){
+        return "Grade C"
+    }
+    else if(marks >= 60 && marks <80){
+        return "Grade B";
+    }
+    else if(marks>=80 && marks <=100){
+        return "Grade A";
+    }
+    else{
+        return "Not a valid number";
+    }
+}
+console.log(studentMarks(102))
